06a0b0c5b431b50405f1bdfff51f8a33233932e1
Artem Teslenko <ates@ipv6.dp.ua>
2024-01-27 20:22:07 +0200

    Add more ops

--- src/wg.erl
+++ src/wg.erl
@@ -3,9 +3,18 @@
 -export([genkey/0]).
 -export([genpsk/0]).
 -export([pubkey/1]).
+-export([add_peer/2]).
 -export([add_peer/3]).
+-export([remove_peer/1]).
 -export([remove_peer/2]).
+-export([peers/0]).
 -export([peers/1]).
+-export([dump/0]).
+-export([dump/1]).
+-export([peer/1]).
+-export([peer/2]).
+-export([is_peer_connected/1]).
+-export([is_peer_connected/2]).
 
 genkey() ->
     {0, Key} = exec(<<"wg genkey">>), Key.
@@ -15,6 +24,9 @@ genpsk() ->
 
 pubkey(PrivKey) when is_binary(PrivKey) ->
     {0, PubKey} = exec(<<"echo ", PrivKey/binary, " | wg pubkey">>), PubKey.
+
+add_peer(PubKey, AllowedIPs) ->
+    add_peer(ifname(), PubKey, AllowedIPs).
 
 add_peer(Ifname, PubKey, AllowedIPs) ->
     Cmd = iolist_to_binary([
@@ -31,6 +43,9 @@ add_peer(Ifname, PubKey, AllowedIPs) ->
             {error, Reason}
     end.
 
+remove_peer(PubKey) ->
+    remove_peer(ifname(), PubKey).
+
 remove_peer(Ifname, PubKey) ->
     Cmd = iolist_to_binary([
         "sudo wg set ",
@@ -45,6 +60,9 @@ remove_peer(Ifname, PubKey) ->
             {error, Reason}
     end.
 
+peers() ->
+    peers(ifname()).
+
 peers(Ifname) ->
     Cmd = iolist_to_binary([
         "sudo wg show ",
@@ -56,6 +74,53 @@ peers(Ifname) ->
             {ok, binary:split(Data, <<"\n">>, [global, trim_all])};
         {_Code, Reason} ->
             {error, Reason}
+    end.
+
+dump() ->
+    dump(ifname()).
+
+dump(Ifname) ->
+    Cmd = iolist_to_binary([
+        "sudo wg show ",
+        Ifname,
+        " dump | tail -n +2"
+    ]),
+    {0, Data} = exec(Cmd),
+    lists:foldl(
+        fun(Line, Acc) ->
+            [PubKey, _PreKey, _EP, _AI, LH, Rx, Tx, KA] = binary:split(Line, <<"\t">>, [global, trim_all]),
+            Acc#{
+                PubKey => #{
+                    lh => binary_to_integer(LH), %% latest-handshake
+                    rx => binary_to_integer(Rx), %% transfer-rx
+                    tx => binary_to_integer(Tx), %% transfer-tx
+                    ka => case KA of             %% persistent-keepalive
+                        <<"off">> -> 0;
+                        _ -> binary_to_integer(KA)
+                    end
+                }
+            }
+        end,
+        #{},
+        binary:split(Data, <<"\n">>, [global, trim_all])
+    ).
+
+
+peer(PubKey) ->
+    peer(ifname(), PubKey).
+
+peer(Ifname, PubKey) ->
+    maps:get(PubKey, dump(Ifname), undefined).
+
+is_peer_connected(PubKey) ->
+    is_peer_connected(ifname(), PubKey).
+
+is_peer_connected(Ifname, PubKey) ->
+    Now = erlang:system_time(second),
+    case maps:get(PubKey, dump(Ifname), undefined) of
+        #{lh := LH} when Now - LH < 180 ->
+            true;
+        _ -> false
     end.
 
 exec(Command) ->
@@ -76,4 +141,12 @@ get_data(Port, Acc, Timeout) ->
             {ExitCode, string:trim(Acc)}
     after Timeout ->
         {1, Acc}
+    end.
+
+ifname() ->
+    case persistent_term:get({?MODULE, ifname}, undefined) of
+        undefined ->
+            persistent_term:put({?MODULE, ifname}, application:get_env(?MODULE, ifname, <<"wg0">>)),
+            ifname();
+        Ifname -> Ifname
     end.
